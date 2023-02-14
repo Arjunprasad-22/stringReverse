bf85227ceb9e17b6e86df5367daa69a32a4f998c
Arjunprasad P M <arjunprasadpm22@gmail.com>
2023-02-14 16:37:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Swapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine("Reversed string: " + new string(charArray));
+            Console.ReadLine();
+        }
+    }
+}
